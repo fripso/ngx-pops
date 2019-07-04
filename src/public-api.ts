70aaf95f4754ada952d899d4979559d546809646
Friso Hoekstra <friso@gunsforhire.nl>
2019-07-04 12:01:40 +0200

    remove deprecated export
    Signed-off-by: Friso Hoekstra <friso@gunsforhire.nl>

--- src/public-api.ts
+++ src/public-api.ts
@@ -1,5 +1,4 @@
 export * from './lib/pops.service';
-export * from './lib/set-position.directive';
 export * from './lib/pops-container.component';
 export * from './lib/pop.component';
 export * from './lib/ngx-pops.module';
